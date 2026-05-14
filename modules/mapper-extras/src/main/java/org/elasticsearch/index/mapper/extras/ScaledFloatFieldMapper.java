5936953f37285cb00ce3631981dc21cfd7e27a61
gmarouli <mary.gouseti@elastic.co>
2026-05-14 18:53:28 +0300

    Update ScaledFloatFieldMapper

--- modules/mapper-extras/src/main/java/org/elasticsearch/index/mapper/extras/ScaledFloatFieldMapper.java
+++ modules/mapper-extras/src/main/java/org/elasticsearch/index/mapper/extras/ScaledFloatFieldMapper.java
@@ -10,8 +10,7 @@
 package org.elasticsearch.index.mapper.extras;
 
 import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.search.DoubleValues;
-import org.apache.lucene.search.LongValues;
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.common.Explicit;
 import org.elasticsearch.common.settings.Setting;
@@ -19,7 +18,6 @@ import org.elasticsearch.common.xcontent.support.XContentMapValues;
 import org.elasticsearch.index.IndexMode;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.IndexVersions;
-import org.elasticsearch.index.fielddata.FieldData;
 import org.elasticsearch.index.fielddata.FieldDataContext;
 import org.elasticsearch.index.fielddata.IndexFieldData;
 import org.elasticsearch.index.fielddata.IndexNumericFieldData;
@@ -845,22 +843,8 @@ public class ScaledFloatFieldMapper extends FieldMapper {
         @Override
         public SortedNumericDoubleValues getDoubleValues() {
             final SortedNumericLongValues values = scaledFieldData.getLongValues();
-            final LongValues singleValues = SortedNumericLongValues.unwrapSingleton(values);
-            if (singleValues != null) {
-                return FieldData.singleton(new DoubleValues() {
-                    @Override
-                    public boolean advanceExact(int doc) throws IOException {
-                        return singleValues.advanceExact(doc);
-                    }
-
-                    @Override
-                    public double doubleValue() throws IOException {
-                        return singleValues.longValue() * scalingFactorInverse;
-                    }
-                });
-            } else {
-                return new SortedNumericDoubleValues() {
-
+            if (values.isSingleton()) {
+                return new SortedNumericDoubleValues.Singleton() {
                     @Override
                     public boolean advanceExact(int target) throws IOException {
                         return values.advanceExact(target);
@@ -872,11 +856,33 @@ public class ScaledFloatFieldMapper extends FieldMapper {
                     }
 
                     @Override
-                    public int docValueCount() {
-                        return values.docValueCount();
+                    public DocIdSetIterator iterator() {
+                        return values.iterator();
                     }
                 };
             }
+            return new SortedNumericDoubleValues() {
+
+                @Override
+                public boolean advanceExact(int target) throws IOException {
+                    return values.advanceExact(target);
+                }
+
+                @Override
+                public double nextValue() throws IOException {
+                    return values.nextValue() * scalingFactorInverse;
+                }
+
+                @Override
+                public int docValueCount() {
+                    return values.docValueCount();
+                }
+
+                @Override
+                public DocIdSetIterator iterator() {
+                    return values.iterator();
+                }
+            };
         }
     }
 
